58d810f348480ef69bf283d65695bc7f2f23b719
Terran Olson <terran@halfcadence.net>
2022-01-28 17:12:11 -0800

    adding usage note

--- pgrs.sh
+++ pgrs.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
+if [ $# -lt 3 ]; then
+    echo "Usage: pgrs [USER] [DATABASE] [FILE]"
+    exit
+fi
+
+
 psql -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public; GRANT ALL ON SCHEMA public TO $1;" $2 postgres
 pg_restore -U postgres --no-password --role "$1" --dbname "$2" --schema public \
 	--no-owner --no-privileges --no-comments -1 "$3"
